f035bfa968d73c44e710fac9be01c38d7428a382
leonardops <leonardops@leonardops-Aspire-E5-573G>
2016-11-17 08:34:09 -0200

    11.1. Validando se existe um estilo persistido

--- Curso_Projeto_Brewer-Spring_Framework_Expert-Algaworks/src/main/java/com/algaworks/brewer/controller/EstilosController.java
+++ Curso_Projeto_Brewer-Spring_Framework_Expert-Algaworks/src/main/java/com/algaworks/brewer/controller/EstilosController.java
@@ -2,30 +2,42 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Estilo;
+import com.algaworks.brewer.service.CadastroEstiloService;
+import com.algaworks.brewer.service.exception.NomeEstiloJacadastradoException;
 
 @Controller
 public class EstilosController {
+
+	@Autowired
+	private CadastroEstiloService cadastroEstiloService;
 	
 	@RequestMapping("/estilos/novo")
-	public String novo(Estilo estilo) {
-		return "estilo/CadastroEstilo"; // Forward encaminha direto e já retorna (Padrão) 
+	public ModelAndView novo(Estilo estilo) {
+		return new ModelAndView("estilo/CadastroEstilo");
 	}
 	
 	@RequestMapping(value = "/estilos/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Estilo estilo, BindingResult result, Model model, RedirectAttributes attributes) {  
-		if(result.hasErrors()){ // verifica erro
+	public ModelAndView cadastrar(@Valid Estilo estilo, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
 			return novo(estilo);
 		}
-		attributes.addFlashAttribute("mensagem", "Estilo salvo com sucesso!");
-		return "redirect:/estilos/novo"; // Redirect força o navegador a fazer uma nova requisição e começar tudo de novo
+		try{
+			cadastroEstiloService.salvar(estilo);
+		} catch (NomeEstiloJacadastradoException e) {
+			result.rejectValue("nome", e.getMessage(), e.getMessage());
+			return novo(estilo);
+		}
+		attributes.addFlashAttribute("mensagem", "Estilo salvo com sucesso");
+		return new ModelAndView("redirect:/estilos/novo");
 	}
 	
 }
---
+++ Curso_Projeto_Brewer-Spring_Framework_Expert-Algaworks/src/main/java/com/algaworks/brewer/service/exception/NomeEstiloJacadastradoException.java
@@ -0,0 +1,10 @@
+package com.algaworks.brewer.service.exception;
+
+public class NomeEstiloJacadastradoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public NomeEstiloJacadastradoException(String message){
+		super(message);
+	}
+}
