d3b891676ddd688e2aea9ab200c6cd3feb0be374
leonardops <leonardops@leonardops-Aspire-E5-573G>
2016-12-16 10:08:33 -0200

    14.5. Salvando o nome da foto da cerveja, 14.4. Melhorando a disponibilidade da aplicação - retorno assíncrono

--- Curso_Projeto_Brewer-Spring_Framework_Expert-Algaworks/src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
+++ Curso_Projeto_Brewer-Spring_Framework_Expert-Algaworks/src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
@@ -3,6 +3,8 @@ package com.algaworks.brewer.config.init;
 import java.util.ServiceConfigurationError;
 
 import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration.Dynamic;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
@@ -35,6 +37,11 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 		characterEncodingFilter.setForceEncoding(true);
 		
 		return new Filter[] { characterEncodingFilter };
+	}
+	
+	@Override
+	protected void customizeRegistration(Dynamic registration) {
+		registration.setMultipartConfig(new MultipartConfigElement(""));
 	}
 
 }
